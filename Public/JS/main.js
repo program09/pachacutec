b07af508d889e7f6723bcd38a18ee14a9631aff2
program09 <94986348+program09@users.noreply.github.com>
2023-01-18 21:25:12 -0500

    Add files via upload

---
+++ Public/JS/main.js
@@ -0,0 +1,76 @@
+
+        function grids(){
+            const button = [...document.querySelectorAll(".rows")];
+            button.forEach(function(elemento, index, arreglo) {
+                arreglo[0].classList.add("grid-4");
+                arreglo[1].classList.add("grid-4");
+                arreglo[2].classList.add("grid-4");
+                arreglo[3].classList.add("grid-4");
+                arreglo[4].classList.add("grid-4");
+                arreglo[5].classList.add("grid-4");
+                arreglo[6].classList.add("grid-4");
+                arreglo[7].classList.add("grid-4");
+                arreglo[8].classList.add("grid-4");
+                arreglo[9].classList.add("grid-4");
+                arreglo[10].classList.add("grid-4");
+                arreglo[11].classList.add("grid-4");
+                arreglo[12].classList.add("grid-4");
+                arreglo[13].classList.add("grid-4");
+                arreglo[14].classList.add("grid-4");
+                arreglo[15].classList.add("grid-4");
+                arreglo[16].classList.add("grid-4");
+                arreglo[17].classList.add("grid-4");
+                arreglo[18].classList.add("grid-4");
+                arreglo[19].classList.add("grid-4");
+                arreglo[20].classList.add("grid-4");
+                arreglo[21].classList.add("grid-4");
+                arreglo[22].classList.add("grid-4");
+                arreglo[23].classList.add("grid-4");
+                arreglo[24].classList.add("grid-4");
+                arreglo[25].classList.add("grid-4");
+                arreglo[26].classList.add("grid-4");
+                arreglo[27].classList.add("grid-4");
+                arreglo[28].classList.add("grid-4");
+
+            });
+        }
+        function gridsred(){
+            const gridacont = document.getElementById("adsd_sdf");
+            const button = [...document.querySelectorAll(".rows")];
+            button.forEach(function(elemento, index, arreglo) {
+                arreglo[0].classList.remove("grid-4");
+                arreglo[1].classList.remove("grid-4");
+                arreglo[2].classList.remove("grid-4");
+                arreglo[3].classList.remove("grid-4");
+                arreglo[4].classList.remove("grid-4");
+                arreglo[4].classList.remove("grid-4");
+                arreglo[5].classList.remove("grid-4");
+                arreglo[6].classList.remove("grid-4");
+                arreglo[7].classList.remove("grid-4");
+                arreglo[8].classList.remove("grid-4");
+                arreglo[9].classList.remove("grid-4");
+                arreglo[10].classList.remove("grid-4");
+                arreglo[11].classList.remove("grid-4");
+                arreglo[12].classList.remove("grid-4");
+                arreglo[13].classList.remove("grid-4");
+                arreglo[14].classList.remove("grid-4");
+                arreglo[15].classList.remove("grid-4");
+                arreglo[16].classList.remove("grid-4");
+                arreglo[17].classList.remove("grid-4");
+                arreglo[18].classList.remove("grid-4");
+                arreglo[19].classList.remove("grid-4");
+                arreglo[20].classList.remove("grid-4");
+                arreglo[21].classList.remove("grid-4");
+                arreglo[22].classList.remove("grid-4");
+                arreglo[23].classList.remove("grid-4");
+                arreglo[24].classList.remove("grid-4");
+                arreglo[25].classList.remove("grid-4");
+                arreglo[26].classList.remove("grid-4");
+                arreglo[27].classList.remove("grid-4");
+                arreglo[28].classList.remove("grid-4");
+
+            });
+        }
+        
+
+
